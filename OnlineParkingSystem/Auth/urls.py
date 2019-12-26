812fd00528cc6a9571958cd0e69daa10df0b8240
nemishzalavadiya <13101999znemish@gmail.com>
2019-12-26 23:30:36 +0530

    login_class

--- OnlineParkingSystem/Auth/urls.py
+++ OnlineParkingSystem/Auth/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     url(r'registration/', views.RegistrationView.as_view(), name="registration"),
     url(r'login/', views.LoginView.as_view(), name="login"),
-    url(r'', views.LoginView.as_view(), name="login")
+    url(r'', views.LoginView.as_view(), name="login"),
+    url(r'loginClass/', views.LoginViewClass.as_view(),name='LoginViewClass'),
 ]
--- OnlineParkingSystem/Auth/views.py
+++ OnlineParkingSystem/Auth/views.py
@@ -3,6 +3,7 @@ from django.template.context_processors import csrf
 from Auth.models import User_detail
 from django.core.mail import send_mail
 from django.conf import settings
+from .forms import User_detailForm
 from django.views.generic import TemplateView
 from django.http import HttpResponse, HttpResponseRedirect
 import math, random
@@ -20,9 +21,10 @@ class LoginView(TemplateView):
         password = request.POST.get('pass')
         if(User_detail.objects.filter(email=email,password=password)):
             print("yes")
+            return render(request,'login.html',{'message':'Login Successful'})
         else:
             print("no")
-        return render(request, 'Login.html')
+            return render(request, 'Login.html',{'message':'Login Failed'})
 
 class RegistrationView(TemplateView):
     def get(self, request, **kwargs):
@@ -38,7 +40,23 @@ class RegistrationView(TemplateView):
         password = request.POST.get('pass')
         if password == cpass:
             User_detail.objects.create(name=fullname,email=email,mobile_no=mobile,password=password,age=age,role="user")
+            return render(request,'login.html',{'message':'Registration Successful'})
         else:
             print("not same")
+            return render(request, 'Registration.html',{'message':'Registration Failed'})
+        
+class LoginViewClass(TemplateView):
+    def get(self, request, **kwargs):
+        c = {}
+        c.update(csrf(request))
+        form=User_detailForm()
+        return render(request, 'login_class_edit.html',{'form':form})
 
-        return render(request, 'Registration.html')
+    def post(self, request, **kwargs):
+        email = request.POST.get('email') 
+        password = request.POST.get('pass')
+        if(User_detail.objects.filter(email=email,password=password)):
+            print("yes")
+        else:
+            print("no")
+        return render(request, 'Login.html')
