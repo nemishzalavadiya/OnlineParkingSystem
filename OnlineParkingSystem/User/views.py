97d8d94f0265f7a038485b273cfbb2a4fc8c6e0f
kevaltalaviya <kevaltalaviya122@gmail.com>
2020-01-03 15:07:51 +0530

    location of land

--- OnlineParkingSystem/User/views.py
+++ OnlineParkingSystem/User/views.py
@@ -5,6 +5,7 @@ from django.core.mail import send_mail
 from django.conf import settings
 from .forms import RegistrationForm,LoginForm
 from django.views.generic import TemplateView
+
 from django.http import HttpResponse, HttpResponseRedirect
 from django.db import models
 from django.template import loader
