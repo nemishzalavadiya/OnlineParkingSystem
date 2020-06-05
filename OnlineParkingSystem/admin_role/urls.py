1ab4ad00bb1fbb9e896056227cf2309db7fccde7
nemishzalavadiya <13101999znemish@gmail.com>
2020-06-05 15:38:19 +0530

    Data Visualization

--- OnlineParkingSystem/admin_role/urls.py
+++ OnlineParkingSystem/admin_role/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     url(r'newLandList/', views.UserListView.as_view(), name="UserListView"),
     url(r'sortLand/', views.UserShortedView.as_view(id=id), name="UserShortedView"),
     url(r'approveLand/', views.UserApproved, name="ApprovedView"),
+    url(r'getScatterdata/', views.getScatterdata, name="getScatterdata"),
+    url(r'getHistdata/', views.getHistdata, name="getHistdata"),
 ]
